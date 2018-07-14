1e64cbc9a747657512a930a8f3018388fa96c98f
Mohammed O. Tillawy <tillawy@gmail.com>
2018-07-14 22:41:44 +0300

    POC socket

--- EasyDisplay/ViewController.swift
+++ EasyDisplay/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import WebKit
 import SnapKit
+import SocketIO
 
 class ViewController: UIViewController, WKUIDelegate {
 
@@ -16,6 +17,19 @@ class ViewController: UIViewController, WKUIDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadWebView()
+        connectSocket()
+    }
+    
+    @IBAction func ibActionDelme() {
+        guard let manager = manager else {
+            return
+        }
+        let socket = manager.defaultSocket
+        socket.emit("event_to_server", ["asdf" : "asdf"])
+    }
+    
+    func loadWebView(){
         
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
@@ -25,7 +39,7 @@ class ViewController: UIViewController, WKUIDelegate {
         webView.uiDelegate = self
         view.addSubview(webView)
         webView.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(view)
+            make.top.equalTo(view).offset(100)
             make.left.equalTo(view)
             make.bottom.equalTo(view)
             make.right.equalTo(view)
@@ -37,6 +51,35 @@ class ViewController: UIViewController, WKUIDelegate {
         
     }
 
-
+    var manager : SocketManager?
+    func connectSocket(){
+        
+        manager = SocketManager(socketURL: URL(string: "http://localhost:8999")!, config: [.log(true), .compress])
+        guard let manager = manager else {
+            return
+        }
+        let socket = manager.defaultSocket
+        
+        socket.on(clientEvent: .connect) {data, ack in
+            print("socket connected")
+        }
+        
+        socket.on("event_to_client")  {data, ack in
+            print("event_to_client ....",data)
+            socket.emit("event_to_server", ["asdf" : "asdf"])
+        }
+        socket.on("currentAmount") {data, ack in
+            guard let cur = data[0] as? Double else { return }
+            
+            socket.emitWithAck("canUpdate", cur).timingOut(after: 0) {data in
+                socket.emit("update", ["amount": cur + 2.50])
+            }
+            
+            ack.with("Got your currentAmount", "dude")
+        }
+        
+        socket.connect()
+    }
+    
 }
 
